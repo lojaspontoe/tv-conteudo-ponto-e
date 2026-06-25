efaf1010e34d0eb81a288e17f5d060e4c5e124d0
lojaspontoe <gptlojaspontoe@gmail.com>
2026-06-25 12:33:59 -0300

    Adiciona configuração do player

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  repositorioGitHub: "lojaspontoe/tv-conteudo-ponto-e",
+  pastaMidia: "media",
+  duracaoFotoSegundos: 7,
+  duracaoVideoSegundos: 30,
+  intervaloAtualizacaoMinutos: 2,
+  rotacaoGraus: 270,
+  modoTeste: false,
+};
